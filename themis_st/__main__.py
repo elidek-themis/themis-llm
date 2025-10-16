72869a34558ec48099d5e710a58cf5042057cf64
karanikolopoulos <ckaranikolopoulos@gmail.com>
2025-10-16 14:59:01 +0000

    Add additional arguments to streamlit entrypoint

--- themis_st/__main__.py
+++ themis_st/__main__.py
@@ -3,10 +3,13 @@ import subprocess
 
 from pathlib import Path
 
+APP_PATH = Path(__file__).parent / "app.py"
+
 
 def run_app():
-    app_path = Path(__file__).parent / "app.py"
-    subprocess.run([sys.executable, "-m", "streamlit", "run", app_path], check=True)
+    args = sys.argv[1:]  # forward any additional arguments to streamlit run
+    cmd = [sys.executable, "-m", "streamlit", "run", str(APP_PATH)] + args
+    subprocess.run(cmd, check=True)
 
 
 if __name__ == "__main__":
